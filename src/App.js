02e97af7407d342f2338865fee8848f9b5307d16
s1hagog <47111905+s1hagog@users.noreply.github.com>
2019-12-20 02:42:34 -0800

    map func

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Define list of items
+
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },{
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramov, ANdrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 0,
+  }
+]
+
+
+
+
+
+class App extends Component {
+  render(){
+    const helloWorld = 'Hello Sooka shlyushka';
+    const lorem = 'Dolor elit Lorem culpa reprehenderit ad enim cillum deserunt ad.';
+
+    const person = {firstName: 'alex', lastName: 'moshak'};
+    return(
+      <div className="App">
+        <h2>{helloWorld}</h2>
+        <h3>{lorem}</h3>
+        <span>The author is {person.firstName} and {person.lastName}</span>
+      </div>
+    )
+  }
 }
 
 export default App;
