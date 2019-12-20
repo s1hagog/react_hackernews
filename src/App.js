3e1298a4db14fa99b8d54933fab27cd5055954b4
s1hagog <47111905+s1hagog@users.noreply.github.com>
2019-12-20 09:13:41 -0800

    restructured app class to have reusable classes

--- src/App.js
+++ src/App.js
@@ -70,14 +70,42 @@ class App extends Component {
     
     return(
       <div className="App">
-        <form>
-          <input 
-            type="text"
-            onChange={this.onSearchChange}
-            value={searchTerm}
-          />
-        </form>
-        {list.filter(isSearched(searchTerm)).map(item =>
+        <Search
+          value={searchTerm}
+          onChange={this.onSearchChange}
+        />
+        <Table 
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
+      </div>
+    )
+  }
+}
+
+
+class Search extends Component {
+  render() {
+    const {value, onChange} = this.props;
+    return(
+      <form>
+        <input 
+          type="text"
+          onChange={onChange}
+          value={value}
+        />
+      </form>
+    )
+  }
+}
+
+class Table extends Component {
+  render() {
+    const {list, pattern, onDismiss} = this.props;
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map(item =>
           <div key={item.objectID}>
             <span><a href={item.url}>{item.title}</a></span>
             <span>{item.author}</span>
@@ -85,14 +113,14 @@ class App extends Component {
             <span>{item.points}</span>
             <span>
               <button
-                onClick={() => this.onDismiss(item.objectID)}
+                onClick={() => onDismiss(item.objectID)}
                 type="button"
               >
                 Dismiss
               </button>
             </span>
           </div>
-          )}
+        )}
       </div>
     )
   }
